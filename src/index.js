67c219017a18b72cbb62fbe25c3a66ba3562170c
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-11-10 12:12:20 +0500

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const numbers = [1, 2, 3];
 
 const sum = (numbers = [0]) => {
     return numbers.reduce((acc, num) => {
-        return acc += num;
+        return acc = acc + num;
     }, 0);
 };
 
